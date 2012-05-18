3008c35412898743d2959ad2427fc52b85c3b3fb
Alex <stephenalexbrowne@gmail.com>
2012-05-18 14:58:36 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,13 @@ SampleApp::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
 
+  # -- Auth --
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
